461de9278e6b98f886bf4ea61e784de70d3ef503
Aniketh01 <anikethgireesh@gmail.com>
2019-03-11 18:43:54 +0530

    Snippet codes related to DNS resolver added as misc

---
+++ snippets/test3.py
@@ -0,0 +1,37 @@
+from twisted.internet.protocol import Factory, Protocol
+from twisted.internet import reactor
+from twisted.names import dns
+from twisted.names import client, server
+
+
+CHANGE = 'example.com'
+TO = '127.0.0.1'
+TTL = 60
+
+class DNSServerFactory(server.DNSServerFactory):
+    def gotResolverResponse(self, (ans, auth, add), protocol, message, address):
+        qname = message.queries[0].name.name
+        if CHANGE in qname:
+            for answer in ans:
+                if answer.type != dns.A:
+                    continue
+                if CHANGE not in answer.name.name:
+                    continue
+                
+                answer.payload.address = socket.inet_aton(TO)
+                answer.payload.ttl = TTL
+
+        args = (self, (ans, auth, add), protocol, message, address)
+        return server.DNSServerFactory.gotResolverResponse(*args)
+
+
+verbosity = 0
+
+resolver = client.Resolver(servers=[('4.2.2.2', 53)])
+factory = DNSServerFactory(clients=[resolver], verbose=verbosity)
+protocol = dns.DNSDatagramProtocol(factory)
+factory.noisy = protocol.noisy = verbosity
+
+reactor.listenUDP(53, protocol)
+reactor.listenTCP(53, factory)
+reactor.run()
---
+++ snippets/test4.py
@@ -0,0 +1,7 @@
+import dns.resolver #import the module
+
+myResolver = dns.resolver.Resolver() #create a new instance named 'myResolver'
+myAnswers = myResolver.query("google.com", "NS") #Lookup the 'A' record(s) for
+for rdata in myAnswers.response.additional: #for each response
+        print (rdata) #print the data
+        
